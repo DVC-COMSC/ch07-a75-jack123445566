19616b2e79bd34e3da243fb88c2e40db30c1cd0b
jack123445566 <112512759+jack123445566@users.noreply.github.com>
2022-12-13 23:53:20 +0000

    2nd commit

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 
-import sys
 
-num1 = list(map(int, input().split()))
-num2 = list(map(int, input().split()))
+num1 = list(map(int, input("Enter the elements of num1 separated by space: ").split()))
+num2 = list(map(int, input("Enter the elements of num2 separated by space: ").split()))
 
-if len(num1) > len(num2):
-	print ('False')
-	sys.exit(0)
-for i in range(len(num2)-len(num1)+1):
-	for j in range(len(num1)):
-		if num1[j] != num2[i+j]:
-			print ('Not')
-			break
-	else:
-			print ('subset')
+
+if all(x in num2 for x in num1):
+    
+    for i in range(len(num1)):
+        if num1[i] != num2[i]:
+            print("False")
+            break
+    else:
+        print("True")
+else:
+    print("False")
